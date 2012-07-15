e6d2b7479f171ca81382455adc3788a3f8427d98
Iwan Gabovitch <qubodup@gmail.com>
2012-07-15 19:04:03 +0200

    pedestrians bounce from walls

--- lib/pedestrian.rb
+++ lib/pedestrian.rb
@@ -8,4 +8,22 @@ class Pedestrian < Unit
     :normal
   end
   
+  def update_movement(dt)
+    # Invert direction if outside of field and put inside field
+    if @pos.x > 776
+      @pos.x = 776
+      @vel.x = - @vel.x
+    elsif @pos.x < 24
+      @pos.x = 24
+      @vel.x = - @vel.x
+    end
+    if @pos.y > 576
+      @pos.y = 576
+      @vel.y = - @vel.y
+    elsif @pos.y < 24
+      @pos.y = 24
+      @vel.y = - @vel.y
+    end
+  end
+
 end
